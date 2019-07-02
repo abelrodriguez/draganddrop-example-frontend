bca8cb0302876eeb6115544deb8894eae2251c76
abelrodriguez <tecnico@abelrodriguez.es>
2019-07-02 18:37:38 +0200

    feat: words list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { WordsListComponent } from '../components/words-list/words-list.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        WordsListComponent
     ],
     imports: [
         BrowserModule
